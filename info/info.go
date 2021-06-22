fbe72efa8f1f891ceea35418fd6d0ac91ccea6c9
Starlight Romero <starlightromero@protonmail.com>
2021-06-21 19:42:30 -0700

    [update] info.go
    add PrintWelcome func

--- info/info.go
+++ info/info.go
@@ -1,8 +1,15 @@
 package info
 
+import "fmt"
+
 const (
-	MainTitle    = "BMI Calculator"
-	Separator    = "------------------"
+	mainTitle    = "BMI Calculator"
+	separator    = "------------------"
 	WeightPrompt = "Please enter your weight (lbs): "
 	HeightPrompt = "Please enter your height (in): "
 )
+
+func PrintWelcome() {
+	fmt.Println(mainTitle)
+	fmt.Println(separator)
+}
