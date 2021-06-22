d74e85bfbca995a98c504f1bafe63a3bf0b46908
Starlight Romero <starlightromero@protonmail.com>
2021-06-21 19:43:01 -0700

    [update] input.go
    add getUserMetrics and getUserInput funcs

--- input.go
+++ input.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/starlightromero/bmi-calc/info"
 )
 
 var (
 	reader = bufio.NewReader(os.Stdin)
 )
+
+func getUserMetrics() (float64, float64) {
+	weight := getUserInput(info.WeightPrompt)
+	height := getUserInput(info.HeightPrompt)
+
+	return weight, height
+}
+
+func getUserInput(promptText string) float64 {
+	fmt.Print(promptText)
+
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\n", "", -1)
+	enteredValue, _ := strconv.ParseFloat(userInput, 64)
+
+	return enteredValue
+}
