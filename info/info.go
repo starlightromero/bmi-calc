175e3a2fce21f25a6c3ee59c0fd2ba5689c782f8
Starlight Romero <starlightromero@protonmail.com>
2021-06-21 17:16:51 -0700

    [create] info.go

---
+++ info/info.go
@@ -0,0 +1,8 @@
+package info
+
+const (
+	MainTitle    = "BMI Calculator"
+	Separator    = "------------------"
+	WeightPrompt = "Please enter your weight (lbs): "
+	HeightPrompt = "Please enter your height (in): "
+)
